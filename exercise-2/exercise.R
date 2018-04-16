9625ec1d4dd3df823ea63bfcef0170dde47eb773
Tristan Alexander Ingold <tingold@uw.edu>
2018-04-16 16:33:29 -0700

    Finish exercise 2 and 4

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -6,38 +6,38 @@
 #install.packages("devtools")
 #devtools::install_github("hadley/fueleconomy")
 library(fueleconomy)
-
+View(vehicles)
 # Install and load the "dplyr" library
 
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+make <- select(vehicles, make)
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
-
+nrow(distinct(vehicles, make))
 
 # Filter the data set for vehicles manufactured in 1997
-
+in_1997 <- filter(vehicles, year == 1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
-
+arrange(in_1997, hwy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+new_in_1997 <- mutate(in_1997, average = (hwy * cty) / 2)
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+new_cars <- filter(vehicles, drive == "2-Wheel Drive", cty > 20)
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
-
+filter <- filter(new_cars, hwy == min(hwy))
+worst_ID <- select(filter, id)
 # Write a function that takes a `year_choice` and a `make_choice` as parameters,
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
@@ -45,4 +45,10 @@ library(fueleconomy)
 
 
 # What was the most efficient Honda model of 1995?
+
+
+# Nested Operation
+best_car_name <- 
+
+
 
--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -2,43 +2,55 @@
 
 # Install the `nycflights13` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-
-
+install.packages("nycflights13")
+library(nycflights13)
+library(dplyr)
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
-
+colnames(flights)
+str(flights)
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
 
+flights <- mutate(flights, amt_time_gain_loss = arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
-
+descending <- arrange(flights, -amt_time_gain_loss)
+head(descending)
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+desc_new <- flights %>% 
+  mutate(amt_time_gain_loss = arr_delay - dep_delay) %>% 
+  arrange(flights, amt_time_gain_loss)
 
 # Make a histogram of the amount of time gained using the `hist()` function
-
+hist(flights$amt_time_gain_loss)
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+mean(flights$amt_time_gain_loss, na.rm = TRUE)
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
-
+seattle_flights <- flights %>% 
+  select(origin, dest, amt_time_gain_loss) %>% 
+  filter(dest == 'SEA')
+  
+View(flights)
 # On average, did flights to SeaTac gain or loose time?
-
+mean(seattle_flights$amt_time_gain_loss, na.rm = TRUE)
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
 # (without showing any other data)!
-
+  filter(flights, origin == "JFK", dest == "SEA") %>% 
+  summarise(
+    avg_air_time = mean(air_time, na.rm = TRUE),
+    max_air_time = max(air_time, na.rm = TRUE),
+    min_air_time = min(air_time, na.rm = TRUE)
+  )
